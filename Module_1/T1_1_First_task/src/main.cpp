67a948fd04776f072b86a1e113a7f2d85ad6bdf2
huazhi.han <huazhi.han@aalto.fi>
2024-01-11 22:24:21 +0200

    things done.

--- Module_1/T1_1_First_task/src/main.cpp
+++ Module_1/T1_1_First_task/src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
 // the main function
-int main(void)
-{
-    /* Copy the code after this comment. */
-        return 0;
+int main(){
+    int a;
+    std::cout << "Hello there!" << std::endl;
+
+    a = 12;
+    std::cout << "a=" << a << std::endl;
+
+    a = 13;
+    std::cout << "a=" << a << std::endl;
+    return 0;
 }
