3b74058e86c97d2136676814552dccfdbd80e4b8
huazhi.han <huazhi.han@aalto.fi>
2024-01-14 12:48:20 +0200

    T2.5

---
+++ Module_2/T2_5_Bank_account/build/CMakeFiles/bank_account.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bank_account.dir/src/bank_account.cpp.o"
+  "CMakeFiles/bank_account.dir/src/bank_account.cpp.o.d"
+  "CMakeFiles/bank_account.dir/src/main.cpp.o"
+  "CMakeFiles/bank_account.dir/src/main.cpp.o.d"
+  "bank_account"
+  "bank_account.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bank_account.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Module_2/T2_5_Bank_account/src/bank_account.cpp
+++ Module_2/T2_5_Bank_account/src/bank_account.cpp
@@ -1,4 +1,55 @@
-
 #include "bank_account.hpp"
+#include <iostream>
+BankAccount::BankAccount(std::string const& owner, std::string const& account_number,
+    double balance) : owner_(owner), account_number_(account_number), balance_(balance){
 
+    }
 
+std::string BankAccount::GetOwner() const
+{
+    return owner_;
+}
+
+std::string BankAccount::GetNumber() const
+{
+    return account_number_;
+}
+
+double BankAccount::GetBalance() const
+{
+    return balance_;
+}
+
+void BankAccount::Deposit(double amount)
+{
+        balance_ += amount;
+
+}
+
+bool BankAccount::Withdraw(double amount)
+{
+    if( amount <= balance_){
+        balance_ -= amount;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void BankAccount::Print() const
+{
+    std::cout << "Account num: " << account_number_ << ", owned by: " << owner_ << ", balance: " << balance_ << " eur." << std::endl;
+}
+
+bool Transfer(BankAccount &source_account, BankAccount &target_account, double amount)
+{
+    if( amount <= source_account.GetBalance()){
+        source_account.Withdraw(amount);
+        target_account.Deposit(amount);
+        return true;
+    }
+    else{
+        return false;
+    }
+}
