a80d373d54c5d422088f1d2e59464de1d8274535
huazhi.han <huazhi.han@aalto.fi>
2024-01-14 16:30:49 +0200

    T2.6

---
+++ Module_2/T2_6_Vectors/build/CMakeFiles/vectors.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vectors.dir/src/main.cpp.o"
+  "CMakeFiles/vectors.dir/src/main.cpp.o.d"
+  "CMakeFiles/vectors.dir/src/vector.cpp.o"
+  "CMakeFiles/vectors.dir/src/vector.cpp.o.d"
+  "vectors"
+  "vectors.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vectors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Module_2/T2_6_Vectors/src/vector.cpp
+++ Module_2/T2_6_Vectors/src/vector.cpp
@@ -4,15 +4,31 @@
 
 int GetMin(std::vector<int> v) {
   
-  return 0; 
+  int min = 1000;
+  for(unsigned int i = 0; i < v.size(); i++){
+      if(v[i] < min){
+        min = v[i];
+      }
+  }
+  return min; 
 }
 
 int GetMax(std::vector<int> v) {
-  
-  return 0; 
+  int max = -1000;
+  for(unsigned int i = 0; i < v.size(); i++){
+      if(v[i] > max){
+        max = v[i];
+      }
+  }
+  return max; 
 }
 
 double GetAvg(std::vector<int> v) {
-  
-  return 0.0; 
+  double sum = 0.0;
+  double avg = 0.0;
+  for(unsigned int i = 0; i < v.size(); i++){
+    sum += v[i];
+  }
+  avg = sum / v.size();
+  return avg;
 }
