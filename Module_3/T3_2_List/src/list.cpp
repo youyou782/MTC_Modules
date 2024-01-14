b8c9d0dcca71fb7b073a73fb78ee53fd9800deec
huazhi.han <huazhi.han@aalto.fi>
2024-01-14 22:20:00 +0200

    T3.2

--- Module_3/T3_2_List/src/list.cpp
+++ Module_3/T3_2_List/src/list.cpp
@@ -5,13 +5,24 @@
 // Implement the functions here
 
 std::istream& GetLines(std::istream& is, std::list<std::string>& list) {
-  
+    std::string temp;
+    while(std::getline(is, temp)){
+        if(temp.back() == '\n'){
+            temp.pop_back();
+        }
+        list.push_back(temp);
+    }
+    return is;
 }
 
 void Print(const std::list<std::string>& list) {
-  
+    for(std::list<std::string>::const_iterator i = list.begin(); i != list.end(); i++){
+        std::cout << *i << std::endl;
+    }
 }
 
 void SortAndUnique(std::list<std::string>& list) {
-  
+    list.sort();
+    list.unique();
+
 }
