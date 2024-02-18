7b4a1ce91fd98df5f25cff91463ce0193f55d064
huazhi.han <huazhi.han@aalto.fi>
2024-02-19 00:41:13 +0200

    Module 7

--- Module_6/T6_6_Event_queue/src/main.cpp
+++ Module_6/T6_6_Event_queue/src/main.cpp
@@ -12,10 +12,11 @@
 #define BLINKING_RATE     500ms
 //---------------END PLACE HOLDER-------------------------/
 
-
+DigitalOut led(LED1);
+DigitalIn button(BUTTON1);
 void eventHandler(int id){
     // complete the function definition
-    std::cout<<id<<std::endl;
+    led = !led;
     
 }
 
@@ -33,9 +34,16 @@ void task_proc(void) {
 
     // Events can be posted multiple times and enqueue gracefully until
     // the dispatch function is called.
-    event.post(1);
-    event.post(2);
-    event.post(3);
+    while(1){
+        
+        if(!button){
+            event.cancel();
+            led = 0;
+        }
+
+        ThisThread::sleep_for(BLINKING_RATE);    
+    }
+
     // event2.post(4);
 
     
@@ -58,7 +66,7 @@ int main()
     // specified in milliseconds
     event.delay(100ms);
     event.period(200ms);
-    
+    event.post(1);
 
     // 3. start the thread, and assign their procedures (functions)
     thread1.start(callback(task_proc));
@@ -66,15 +74,15 @@ int main()
     // 4. Call the queue dispatch function
     // Posted events are dispatched in the context of the queue's
     // dispatch function
-    queue.dispatch_for(1000ms);
+    queue.dispatch_forever();
+    // queue.call_in()
     
     // 5. wait for thread to terminate (if it ever terminates)
     thread1.join();
     // the following code is just a placeholder. You may remove it if you find it fit.
     //---------------BEGIN PLACE HOLDER-------------------------/
-    DigitalOut led(LED1);
     while (true) {
-        led = !led;
+        // led = !led;
         ThisThread::sleep_for(BLINKING_RATE);
     }
     //---------------END PLACE HOLDER-------------------------/
